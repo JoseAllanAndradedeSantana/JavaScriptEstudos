1eb4bb5db15e8b93ab7bbe7271d1af8c63f6f5ef
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-04-20 19:46:43 -0300

    function

---
+++ funcao/cidadaoPrimeiraClasse.js
@@ -0,0 +1,13 @@
+// função em JS é First-Class Object 
+// função Higher order function
+
+//criar de forma literal
+function func1(){
+
+}
+
+// Armazenar em uma variável
+const func2 = function (){}
+
+//Armazenar em um array
+const array = [function(a,b){return a + b}]
