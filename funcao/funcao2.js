18018b7a6769118f1988f5918d1a27917dee95d8
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-04-20 19:47:14 -0300

    function

---
+++ funcao/funcao2.js
@@ -0,0 +1,12 @@
+// this and bind
+
+const pessoa = {
+    saudacao: "Bom dia",
+    falar() {
+        console.log(this.saudacao);
+    }
+}
+
+pessoa.falar();
+const falarDePessoa = pessoa.falar.bind(pessoa);
+falarDePessoa();
