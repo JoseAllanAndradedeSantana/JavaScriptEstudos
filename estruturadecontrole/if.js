a61fcdb5d40a8ad850aa8fcf1deddbc3ed3bcc67
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-04-20 19:45:46 -0300

    if

---
+++ estruturadecontrole/if.js
@@ -0,0 +1,15 @@
+function soBoaNoticia(nota){
+    if(nota >= 7){
+        console.log("Aprovado");
+    }
+}
+
+soBoaNoticia(7);
+
+function seForVerdadeEuFalo(valor){
+    if(valor){
+        console.log("Is true");
+    }
+}
+
+seForVerdadeEuFalo();
