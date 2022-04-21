b554f9dfd200557f1554ae45f26847f98086e6a8
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-04-21 13:17:48 -0300

    estudando array

---
+++ array/array.js
@@ -0,0 +1,10 @@
+let aprovados = new Array("Allan","Gustavo");
+console.log(aprovados)
+aprovados = ["Allan","Gustavo"];
+console.log(aprovados[0]);
+aprovados[3] = "Jose";
+aprovados.push("Andrade");
+console.log(aprovados.length);
+aprovados.sort();
+aprovados.slice(1,2)
+console.log(aprovados);
