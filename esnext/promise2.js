c8f5ac6af5aa85e5d99811fcd08b815c89ef43b9
JoseAllanAndradedeSantana <allan_santana2810@hotmail.com>
2022-04-24 16:32:38 -0300

    promise

---
+++ esnext/promise2.js
@@ -0,0 +1,19 @@
+setTimeout(function () {
+    console.log("Executando callback");
+    setTimeout(function () {
+        console.log("Executando callback");
+
+    }, 1000)
+}, 2000)
+
+
+function esperarPor(tempo = 2000){
+    return new Promise(function(resolve){
+        setTimeout(function(){
+            console.log("Executando Promise")
+            resolve("-------")
+        },tempo)
+    })
+}
+
+esperarPor(3000);
